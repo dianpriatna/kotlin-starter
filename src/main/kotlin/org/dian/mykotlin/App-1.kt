607f0c506be8942a0b406ef8788f12ad3b6c5136
dianpriatna <dian.priatn@gmail.com>
2023-05-04 10:20:28 +0700

    Tambah latihan dari Data Types & Variable

---
+++ src/main/kotlin/org/dian/mykotlin/App-1.kt
@@ -0,0 +1,15 @@
+package org.dian.mykotlin
+
+fun main() {
+    val name = "Alfian"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
+}
+
+/*
+   output:
+       Hello my name is Alfian
+       Always true
+*/
---
+++ src/main/kotlin/org/dian/mykotlin/App_2.kt
@@ -0,0 +1,10 @@
+package org.dian.mykotlin
+
+fun main() {
+    val firstWord = "Dicoding "
+    val lastWord = "Academy"
+    print(firstWord + lastWord)
+}
+/*
+   output: Dicoding Academy
+*/
