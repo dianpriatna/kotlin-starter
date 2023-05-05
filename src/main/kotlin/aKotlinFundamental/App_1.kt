d9ed2ab87ac3135d948ed8aa1ab4b5c741851973
dianpriatna <dian.priatn@gmail.com>
2023-05-05 20:41:08 +0700

    Hello my name is Dian

---
+++ src/main/kotlin/aKotlinFundamental/App_1.kt
@@ -0,0 +1,15 @@
+package aKotlinFundamental
+
+fun main() {
+    val name = "Dian"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
+}
+
+/*
+   output:
+       Hello my name is Dian
+       Always true
+*/
