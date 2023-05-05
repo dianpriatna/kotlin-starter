a361c7310a70366151182ced8c271fa242ce2bcb
dianpriatna <dian.priatn@gmail.com>
2023-05-05 21:00:41 +0700

    Char: Melakukan operasi increment (++) dan decrement (--) pada sebuah variabel dengan tipe data Char

--- src/main/kotlin/aKotlinFundamental/App_1.kt
+++ src/main/kotlin/aKotlinFundamental/App_1.kt
@@ -3,9 +3,9 @@ package aKotlinFundamental
 fun main() {
     val name = "Dian"
 
-    print("Hello my name is ")
+    println("Hello my name is ")
     println(name)
-    print(if (true) "Always true" else "Always false")
+    println(if (true) "Always true" else "Always false")
 }
 
 /*
