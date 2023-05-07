97ac4c9f6586d06d10a8741d08138790ba9ac490
dianpriatna <dian.priatn@gmail.com>
2023-05-07 07:35:52 +0700

    Indexing satu character dengan []

---
+++ src/main/kotlin/aKotlinFundamental/App_9.kt
@@ -0,0 +1,12 @@
+package aKotlinFundamental
+
+fun main() {
+    val text = "Kotlin"
+    val firstChar = text[0]
+
+    print("First character of $text is $firstChar")
+}
+
+/*
+ output : First character of Kotlin is K
+*/
